cce201dd503787dcab73208d1cbce8abeef19cc1
dineshpinto <annual.fallout_0z@icloud.com>
2023-12-24 04:28:20 +0530

    Increased test coverage

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,24 +12,38 @@ def test_networks(api):
     networks = api.networks()
     assert isinstance(networks, dict)
     assert "data" in networks
+    assert "id" in networks["data"][0]
+    assert "type" in networks["data"][0]
+    assert "attributes" in networks["data"][0]
 
 
 def test_dexes(api):
-    dexes = api.dexes(network="eth")
+    dexes = api.network_dexes(network="eth")
     assert isinstance(dexes, dict)
     assert "data" in dexes
+    assert "id" in dexes["data"][0]
+    assert "type" in dexes["data"][0]
+    assert "attributes" in dexes["data"][0]
 
 
 def test_trending_pools(api):
     trending_pools = api.trending_pools()
     assert isinstance(trending_pools, dict)
     assert "data" in trending_pools
+    assert "id" in trending_pools["data"][0]
+    assert "type" in trending_pools["data"][0]
+    assert "attributes" in trending_pools["data"][0]
+    assert "base_token_price_usd" in trending_pools["data"][0]["attributes"]
 
 
 def test_network_trending_pools(api):
     network_trending_pools = api.network_trending_pools(network="eth")
     assert isinstance(network_trending_pools, dict)
     assert "data" in network_trending_pools
+    assert "id" in network_trending_pools["data"][0]
+    assert "type" in network_trending_pools["data"][0]
+    assert "attributes" in network_trending_pools["data"][0]
+    assert "base_token_price_usd" in network_trending_pools["data"][0]["attributes"]
 
 
 def test_network_pool_address(api):
@@ -38,6 +52,9 @@ def test_network_pool_address(api):
     )
     assert isinstance(network_pool_address, dict)
     assert "data" in network_pool_address
+    assert "id" in network_pool_address["data"]
+    assert "type" in network_pool_address["data"]
+    assert "attributes" in network_pool_address["data"]
 
 
 def test_network_pools_multi_address(api):
@@ -50,33 +67,67 @@ def test_network_pools_multi_address(api):
     )
     assert isinstance(network_pools_multi_address, dict)
     assert "data" in network_pools_multi_address
+    assert "included" in network_pools_multi_address
+    assert "id" in network_pools_multi_address["data"][0]
+    assert "type" in network_pools_multi_address["data"][0]
+    assert "attributes" in network_pools_multi_address["data"][0]
 
 
 def test_network_pools(api):
     network_pools = api.network_pools(network="eth")
     assert isinstance(network_pools, dict)
     assert "data" in network_pools
+    assert "id" in network_pools["data"][0]
+    assert "type" in network_pools["data"][0]
+    assert "attributes" in network_pools["data"][0]
 
 
 def test_network_dex_pools(api):
     network_dex_pools = api.network_dex_pools(network="eth", dex="sushiswap")
     assert isinstance(network_dex_pools, dict)
     assert "data" in network_dex_pools
+    assert "id" in network_dex_pools["data"][0]
+    assert "type" in network_dex_pools["data"][0]
+    assert "attributes" in network_dex_pools["data"][0]
 
 
 def test_network_new_pools(api):
     network_new_pools = api.network_new_pools(network="eth")
     assert isinstance(network_new_pools, dict)
     assert "data" in network_new_pools
+    assert "id" in network_new_pools["data"][0]
+    assert "type" in network_new_pools["data"][0]
+    assert "attributes" in network_new_pools["data"][0]
 
 
 def test_new_pools(api):
     new_pools = api.new_pools()
     assert isinstance(new_pools, dict)
     assert "data" in new_pools
+    assert "id" in new_pools["data"][0]
+    assert "type" in new_pools["data"][0]
+    assert "attributes" in new_pools["data"][0]
 
 
 def test_search_network_pool(api):
     search_pools = api.search_network_pool(query="ETH", network="eth")
     assert isinstance(search_pools, dict)
     assert "data" in search_pools
+    assert "id" in search_pools["data"][0]
+    assert "type" in search_pools["data"][0]
+    assert "attributes" in search_pools["data"][0]
+
+
+def test_network_addresses_token_price(api):
+    network_addresses_token_price = api.network_addresses_token_price(
+        network="eth",
+        addresses=[
+            "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+            "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+        ],
+    )
+    assert isinstance(network_addresses_token_price, dict)
+    assert "data" in network_addresses_token_price
+    assert "id" in network_addresses_token_price["data"]
+    assert "type" in network_addresses_token_price["data"]
+    assert "attributes" in network_addresses_token_price["data"]
